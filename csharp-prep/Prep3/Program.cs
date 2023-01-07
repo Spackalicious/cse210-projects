a5ad714ca7e2dd7cf326cbcca360591e8291c164
Julie Spackman <spack84@byui.edu>
2023-01-07 16:09:18 -0700

    Prep 3 almost done except for final stretch

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+
+        Console.WriteLine("Let's play Guess the Magic Number!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1,111);
+
+        int Guess;
+        int i = 0;
+
+        do {
+            Console.Write("What is your guess? ");
+            Guess = int.Parse(Console.ReadLine());
+            
+            if (magicNumber < Guess) 
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (magicNumber > Guess)
+            {
+                Console.WriteLine("Higher");
+            }
+
+            i++;
+        } while (Guess != magicNumber);
+        if (magicNumber == Guess)
+            {
+                Console.WriteLine($"You guessed it! And it only took you {i} tries!");
+                Console.Write("Do you want to play again? Y or N? ");
+                string answer = Console.ReadLine();
+                if (answer == "Y")
+                {
+                    // goto ;
+                    // Application.Restart();
+                    // Program.Main();
+                }
+            }
     }
 }
