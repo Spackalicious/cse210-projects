bf4a54f07dadb47317b18ea3eeaebb3846425391
Julie Spackman <spack84@byui.edu>
2023-02-13 21:12:21 -0700

    Completed Learning 04

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment practice0 = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(practice0.GetSummary());
+        Console.WriteLine();
+
+        MathAssignment practice1 = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(practice1.GetSummary());
+        Console.WriteLine(practice1.GetHomeworkList());
+        Console.WriteLine();
+
+        WritingAssignment practice2 = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(practice2.GetSummary());
+        Console.WriteLine(practice2.GetWritingInformation());
     }
 }
